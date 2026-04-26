480e48f365c15a76cc7b87f84ffd64df12275c7b
Yusuf Ersel Kara <ersel.kara@std.yildiz.edu.tr>
2026-04-27 02:38:44 +0300

    conflict fix vs

--- lib/features/auth/presentation/pages/splash_page.dart
+++ lib/features/auth/presentation/pages/splash_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:sky_app/core/constants/app_assets.dart';
 import 'package:sky_app/features/auth/presentation/providers/user_provider.dart';
+import 'package:sky_app/features/calendar/data/services/event_service.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -19,6 +20,7 @@ class _SplashPageState extends State<SplashPage> {
   }
 
   Future<void> _initApp() async {
+    EventService.prefetch();
     await context.read<UserProvider>().tryAutoLogin();
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,12 @@ import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:sky_app/core/main_app.dart';
 import 'package:sky_app/core/theme/theme_provider.dart';
-<<<<<<< HEAD
-import 'package:sky_app/features/calendar/data/services/event_service.dart';
-
-void main() {
-  EventService.prefetch();
-=======
 import 'package:sky_app/features/auth/presentation/providers/user_provider.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
 void main() {
   timeago.setLocaleMessages('tr', timeago.TrMessages());
 
->>>>>>> upstream/main
   runApp(
     MultiProvider(
       providers: [
